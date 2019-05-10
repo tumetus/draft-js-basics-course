74b243f97455259d8882db483f0e6c8aed20b7a0
Tuomo Kankaanpää <kankaanpaa.tuomo@gmail.com>
2019-05-10 12:23:04 +0300

    Add styles for editor and handleKeyCommand implementation.

--- src/MyEditor.js
+++ src/MyEditor.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 
 class MyEditor extends React.Component {
+    editorStyles = {
+        'width':'200px',
+        'margin':'10px',
+        'border':'1px solid gray'
+    };
     constructor(props) {
         super(props);
 
@@ -10,9 +15,24 @@ class MyEditor extends React.Component {
         };
         this.onChange = (editorState) => this.setState({editorState});
     }
+    handleKeyCommand(command) {
+        const { editorState } = this.state;
+        const newState = RichUtils.handleKeyCommand(editorState, command);
+        if (newState) {
+            this.onChange(newState);
+            return true;
+        }
+        return false;
+    }
     render() {
         return (
-            <Editor editorState={this.state.editorState} onChange={this.onChange} />
+            <div style={this.editorStyles}>
+                <Editor 
+                    editorState={this.state.editorState} 
+                    onChange={this.onChange} 
+                    handleKeyCommand={this.handleKeyCommand.bind(this)} 
+                />
+            </div>
         );
     }
 }
